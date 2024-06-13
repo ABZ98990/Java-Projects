afc66e971ab5621505ae81c3558467d7830874a7
ABZ98990 <master.aggressive@gmail.com>
2024-06-13 23:01:12 +0500

    teacher login page designing

--- QRandomizer/src/Main.java
+++ QRandomizer/src/Main.java
@@ -2,6 +2,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-        new Window();
+        //new Window();
+        new teacher_signup();
     }
 }
--- QRandomizer/src/teacher_signup.java
+++ QRandomizer/src/teacher_signup.java
@@ -6,22 +6,35 @@ public class teacher_signup
     JFrame frame;
     ImageIcon img = new ImageIcon("logo.png");
     JPanel panel;
+    JButton re_btn;
     teacher_signup()
     {
+        re_btn = new JButton("Back");
+        re_btn.setBackground(Color.WHITE);
+        re_btn.setForeground(new Color(0x2B911));
+        re_btn.setVisible(true);
+        re_btn.setFocusable(false);
+        re_btn.setFont(new Font("Arial", Font.BOLD, 16));
+
+
         panel = new JPanel();
         panel.setVisible(true);
         panel.setBackground(new Color(0x2B911));
+        panel.setLayout(null);
+
+        panel.add(re_btn).setBounds(5,5,75,25);
 
 
         frame = new JFrame("Teacher Sign Up");
         frame.setVisible(true);
         frame.setSize(453,680);
         frame.setResizable(false);
+        frame.setLayout(null);
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
         frame.setIconImage(img.getImage());
         frame.setLocationRelativeTo(null);
 
-        frame.add(panel);
+        frame.add(panel).setBounds(0,0,453,680);
     }
 }
