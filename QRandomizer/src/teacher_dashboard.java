38bd6c3fabcf68cd15a509c866a5f90569ebf20a
ABZ98990 <master.aggressive@gmail.com>
2024-06-14 09:23:37 +0500

    teacher object creation and dashboard update

--- QRandomizer/src/teacher_dashboard.java
+++ QRandomizer/src/teacher_dashboard.java
@@ -1,11 +1,27 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class teacher_dashboard extends JFrame
 {
     JFrame frame;
+    JPanel panel;
+    JLabel question;
     ImageIcon img = new ImageIcon("logo.png");
+    JButton logout_btn;
     teacher_dashboard()
     {
+        logout_btn = new JButton("Logout now");
+        logout_btn.setForeground(Color.WHITE);
+        logout_btn.setVisible(true);
+        logout_btn.setFocusable(false);
+        logout_btn.setBackground(new Color(0x2B911));
+
+        panel = new JPanel();
+        panel.setLayout(null);
+        panel.setVisible(true);
+
+        panel.add(logout_btn).setBounds(10,10,100,30);
+
         frame = new JFrame("Dashboard");
         frame.setVisible(true);
         frame.setSize(960,580);
@@ -15,5 +31,7 @@ public class teacher_dashboard extends JFrame
 
         frame.setIconImage(img.getImage());
         frame.setLocationRelativeTo(null);
+
+        frame.add(panel).setBounds(0,0,960,580);
     }
 }
