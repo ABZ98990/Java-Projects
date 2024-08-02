7ca921020bad3b74b4d1937379679e6676babca4
ABZ98990 <master.aggressive@gmail.com>
2024-08-02 02:31:16 -0700

    footnote link to student sign in page

--- QRandomizer/src/Welcome.java
+++ QRandomizer/src/Welcome.java
@@ -9,7 +9,7 @@ public class Welcome extends JFrame implements ActionListener
     JFrame frame;
     ImageIcon img = new ImageIcon("logo.png");
     JPanel panel_one, panel_two;
-    JLabel p2_heading, p1_heading, name, email, password, teacher_login_text, teacher_signup_text, note1, note2;
+    JLabel p2_heading, p1_heading, name, email, password, student_login_note, teacher_login_text, teacher_signup_text, note1, note2;
     JTextField name_field, email_field, pass_field;
     JButton student_signup_btn, teacher_login_btn, teacher_signup_btn;
     Welcome()
@@ -111,6 +111,11 @@ public class Welcome extends JFrame implements ActionListener
         student_signup_btn.setFont(new Font("Arial", Font.BOLD, 16));
         student_signup_btn.addActionListener(this);
 
+        //Student Login Link note
+        student_login_note = new JLabel("Already Signed Up?");
+        student_login_note.setVisible(true);
+
+
 
         //Line 1 of Footnote for username creation in the right panel
         note1 = new JLabel("Note: The username must not contain special characters");
@@ -125,15 +130,16 @@ public class Welcome extends JFrame implements ActionListener
 
         //Adding Fields in the right panel
         panel_two.add(p2_heading).setBounds(190,135,234,30);
-        panel_two.add(name).setBounds(85,200,100,30);
-        panel_two.add(name_field).setBounds(185, 200, 234,30);
-        panel_two.add(email).setBounds(85,250,100,30);
-        panel_two.add(email_field).setBounds(185,250,234,30);
-        panel_two.add(password).setBounds(85, 300, 234, 30);
-        panel_two.add(pass_field).setBounds(185, 300, 234, 30);
-        panel_two.add(student_signup_btn).setBounds(225,350,95,30);
-        panel_two.add(note1).setBounds(70,515,500,120);
-        panel_two.add(note2).setBounds(110,530,500,120);
+        panel_two.add(name).setBounds(85,200,100,30);               //"name"
+        panel_two.add(name_field).setBounds(185, 200, 234,30);      //name_field
+        panel_two.add(email).setBounds(85,250,100,30);              //"email"
+        panel_two.add(email_field).setBounds(185,250,234,30);       //email_field
+        panel_two.add(password).setBounds(85, 300, 234, 30);        //password
+        panel_two.add(pass_field).setBounds(185, 300, 234, 30);     //password_field
+        panel_two.add(student_signup_btn).setBounds(225,350,95,30); //student_signup_button
+        panel_two.add(student_login_note).setBounds(185,400,95,30); //student_login_note
+        panel_two.add(note1).setBounds(70,515,500,120);             //footnote_one
+        panel_two.add(note2).setBounds(110,530,500,120);            //footnote_two
 
 
         //Creating frame window
